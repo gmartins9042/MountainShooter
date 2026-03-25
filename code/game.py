e0b4d272b6ecb983ea3fb2afcff43e68de02c38d
gmartins9042 <gmartins9042@gmail.com>
2026-03-24 21:16:37 -0300

    Creating main loop Connecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame  # Importando o pygame dentro desse arquivo
+
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()  # Está inicializando
+        self.window = pygame.display.set_mode(size=(600, 480))  # Está abrindo uma janela no jogo
 
-    def run(self, ):
-        pass
+    def run(self):
+        print('Setup Start')
+
+        print('Setup Finish')
+
+        print('Loop start')
+        while True:  # Loop no qual vai continuar rodando
+
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+            # Check for all events
+            #for event in pygame.event.get():  # Toda interação está indo para a variável event
+                #if event.type == pygame.QUIT: # Se o event trazer uma variável tipo ele ira encerrar o game
+                    #pygame.quit() # Close window
+                    #quit() # End pygame
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame  # Importando o pygame dentro desse arquivo
+from code.Game import Game
 
-print('Setup Start')
-pygame.init()  # Está inicializando
-window = pygame.display.set_mode(size=(600, 480))  # Está abrindo uma janela no jogo
-print('Setup Finish')
-
-print('Loop start')
-while True:  # Loop no qual vai continuar rodando
-    # Check for all events
-    for event in pygame.event.get():  # Toda interação está indo para a variável event
-        if event.type == pygame.QUIT: # Se o event trazer uma variável tipo ele ira encerrar o game
-            pygame.quit() # Close window
-            quit() # End pygame
+game = Game()
+gamme.run()
